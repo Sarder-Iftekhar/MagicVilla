b0d5e830d354ab5849670a58f71c2260f7b8707d
Sarder-Iftekhar <iftekhar.sarder@gmail.com>
2022-11-26 01:18:04 +0600

    iftekhar

---
+++ MagicVilla_VillaAPI/Repository/VillaRepository.cs
@@ -0,0 +1,51 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaRepository(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public async Task Create(Villa entity)
+        {
+            await _db.Villas.AddAsync(entity); 
+            await Save();
+        }
+
+        public Task<List<Villa>> Get(Expression<Func<Villa,bool>> filter = null, bool tracked = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Villa>> GetAll(Expression<Func<Villa,bool>> filter = null)
+        {
+            IQueryable<Villa> query = _db.Villas;
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+           
+        }
+
+        public async Task Remove(Villa entity)
+        {
+            _db.Villas.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+            
+        }
+    }
+}
